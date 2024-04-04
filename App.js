f3229b0a1d569b02492af1ca722ebb854327a3d7
Hamza Heikal <hheikal.work@gmail.com>
2024-04-04 22:10:04 +0300

    style

--- App.js
+++ App.js
@@ -1,57 +1,72 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  ImageBackground,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
-    <View className="bg-zinc-200" style={styles.container}>
-      <Text
-        style={styles.font}
-        className="text-[#c2a0b0] text-3xl font-extrabold my-10"
-      >
-        {/*cfaabc */}
-        يرجى اختيار موقع الخدمة
-      </Text>
-      <View className="flex-row gap-5">
-        <Pressable
-          className="flex justify-center items-center rounded-full border-2 border-gray-400/20 w-[125] py-4"
-          onPress={() => {}}
+    <ImageBackground
+      className="w-full h-full"
+      source={require("./assets/bg.jpg")}
+      resizeMode="cover"
+    >
+      <View style={styles.container}>
+        <Text
+          style={styles.readexpro}
+          className="text-[#a0667092] text-3xl font-extrabold my-10"
         >
-          <Image
-            className="w-12 h-12 mb-2"
-            source={require("./assets/2162767.png")}
-          />
+          {/*cfaabc */}
+          يرجى اختيار موقع الخدمة
+        </Text>
+        <View className="flex-row gap-5">
+          <Pressable
+            className="flex justify-center items-center rounded-full border-2 border-gray-400/20 w-[125] py-4"
+            onPress={() => {}}
+          >
+            <Image
+              className="w-[63px] h-[63px]"
+              source={require("./assets/4363444.png")}
+            />
 
-          <Text className="text-[#c2a0b0] mt-1 font-bold text-[18px] ">
-            نجيك
-          </Text>
-        </Pressable>
-        <Pressable
-          className="flex justify-center items-center rounded-full border-2 border-gray-400/20 w-[125] py-4"
-          onPress={() => {}}
-        >
-          <Image
-            className="w-[63px] h-[63px]"
-            source={require("./assets/830469.png")}
-          />
-          <Text className="text-[#c2a0b0] mt-1 font-bold text-[18px]">
-            تجينا
-          </Text>
-        </Pressable>
+            <Text className="text-[#a0667092] mt-1 font-bold text-[18px] ">
+              نجيك
+            </Text>
+          </Pressable>
+          <Pressable
+            className="flex justify-center items-center rounded-full border-2 border-gray-400/20 w-[125] py-4"
+            onPress={() => {}}
+          >
+            <Image
+              className="w-[63px] h-[63px]"
+              source={require("./assets/830469.png")}
+            />
+            <Text className="text-[#a0667092] mt-1 font-bold text-[18px]">
+              تجينا
+            </Text>
+          </Pressable>
+        </View>
+        <StatusBar style="auto" />
       </View>
-      <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
-  font: {
+  readexpro: {
     fontFamily: "ReadexPro-Bold",
-    fontSize: 20,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
   },
 });
