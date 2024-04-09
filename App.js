77d86470a8988778c9ab26a1d484f32b0c2b7823
Hamza Heikal <hheikal.work@gmail.com>
2024-04-09 20:28:47 +0300

    Refactor

--- App.js
+++ App.js
@@ -11,24 +11,22 @@ export default function App() {
       <SelectedProvider>
         <Header />
         <ListContainer>
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
+          <ListItem
+            title="خدمة البدي كير لليدين"
+            descreption="توفر هذه الخدمة العناية باليدين (تقليم الأظافر وطلائها)"
+            provider="سارة الثقفي"
+            price="210"
+            offer="30%"
+            image={require("./assets/example.jpeg")}
+            rating={4}
+          />
+          <ListItem
+            title="شسوار الشعر"
+            descreption="تقدم هذه الخدمة سشوار للشعر"
+            provider="سمية الزهراني"
+            price="120"
+            rating={2.3}
+          />
         </ListContainer>
         <NavMenu />
       </SelectedProvider>
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,42 +1,59 @@
-import { Pressable, Text, View } from "react-native";
-import { Heart, Share2, Star } from "lucide-react-native";
+import { Image, Pressable, Text, View } from "react-native";
+import { Heart, ImageOff, Share2, Star } from "lucide-react-native";
 import { useState } from "react";
 
-const ListItem = () => {
+const ListItem = ({
+  title,
+  descreption,
+  price,
+  provider,
+  offer,
+  rating,
+  image,
+}) => {
   const [Liked, setLiked] = useState(false);
   return (
-    <Pressable className="flex-row gap-1 pl-[2px] mx-20 my-1 justify-start items-center border border-gray-200 bg-pink-100/40 w-full h-28 py-1 rounded-2xl">
-      <View className="aspect-square w-[100px] bg-slate-400/10 rounded-xl mb-1"></View>
+    <Pressable className="flex-row gap-1 mx-20 my-1 justify-start items-center border border-gray-200 bg-pink-100/40 w-full h-28 py-1 rounded-2xl">
+      {image ? (
+        <Image
+          className="aspect-square w-[100px] bg-slate-400/10 rounded-xl mb-1 object-cover"
+          source={image}
+        />
+      ) : (
+        <View className="flex items-center justify-center aspect-square w-[100px] bg-slate-400/10 rounded-xl mb-1">
+          <ImageOff className="text-gray-400" />
+        </View>
+      )}
 
       <View className="flex-col w-[69%] items-end text-ellipsis">
         <View className="flex-row-reverse w-full justify-between my-1 h-5">
-          <Text className="text-black truncate w-[73%] font-bold">
-            خدمة البدي كير لليدين
-          </Text>
-          <View className="flex items-center justify-center h-[19]">
-            <Text className="text-white text-[10px] rounded-md bg-rose-600/90 p-[3px]">
-              30% OFFER
-            </Text>
-          </View>
+          <Text className="text-black truncate w-[73%] font-bold">{title}</Text>
+          {offer && (
+            <View className="flex items-center justify-center h-[19]">
+              <Text className="text-white text-[10px] rounded-md bg-rose-600/90 p-[3px]">
+                {offer} OFFER
+              </Text>
+            </View>
+          )}
         </View>
 
         <Text className="text-zinc-600 text-xs truncate h-8">
-          توفر هذه الخدمة العناية باليدين ( تقليم الأظافر وطلائها )
+          {descreption}
         </Text>
 
         <Text className="text-[#633e539b] text-[10px] truncate mb-1">
-          مقدمة الخدمة : سارة الثقفي
+          مقدمة الخدمة : {provider}
         </Text>
 
         <View className="flex-row justify-between w-full">
           <View className="flex-row">
-            {[...Array(3)].map((_, index) => (
+            {[...Array(Math.round(rating))].map((_, index) => (
               <Star
                 fill="rgb(252 211 77)"
                 className="max-w-[16px] max-h-[16px]"
               />
             ))}
-            {[...Array(2)].map((_, index) => (
+            {[...Array(5 - Math.round(rating))].map((_, index) => (
               <Star
                 fill="rgb(209 213 219)"
                 className="max-w-[16px] max-h-[16px]"
@@ -44,9 +61,9 @@ const ListItem = () => {
             ))}
           </View>
 
-          <View className="flex-row-reverse justify-between items-center w-28">
-            <Text className="text-xs text-green-600">210 RS</Text>
-            <View className="flex-row-reverse gap-3">
+          <View className="flex-row-reverse justify-between items-center w-32">
+            <Text className="text-xs text-green-600">{price} RS</Text>
+            <View className="flex-row-reverse gap-4">
               <Pressable onPress={() => setLiked(!Liked)}>
                 <Heart
                   fill={Liked ? "red" : "none"}
