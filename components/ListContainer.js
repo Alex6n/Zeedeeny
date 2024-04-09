928002a69e6cfa9cc1cc0a050dffc2312a3c0626
Hamza Heikal <hheikal.work@gmail.com>
2024-04-09 15:56:45 +0300

    list container style

--- components/ListContainer.js
+++ components/ListContainer.js
@@ -2,7 +2,7 @@ import { ScrollView, View } from "react-native";
 
 const ListContainer = ({ children }) => {
   return (
-    <ScrollView className="w-full h-full mt-[90px] mb-16 px-2">
+    <ScrollView className="w-full h-full mt-[87px] mb-[61px] px-2">
       <View className="w-full flex-1 items-center">{children}</View>
     </ScrollView>
   );
