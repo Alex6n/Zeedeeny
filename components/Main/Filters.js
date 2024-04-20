94fdef0b0d36af0858ec467b6c2ddfce9f29896a
Hamza Heikal <hheikal.work@gmail.com>
2024-04-20 16:13:28 +0300

    refactor, filter progress

---
+++ components/Main/Filters.js
@@ -0,0 +1,74 @@
+import { Modal, Pressable, View } from "react-native";
+import { BoldText } from "../Text/BoldText";
+import { RadioButton, Checkbox } from "react-native-paper";
+import { useState } from "react";
+
+// sort : price, rating
+// filter : liked, offers
+
+export const Filters = ({ modalVisible, setModalVisible }) => {
+  const [checked, setChecked] = useState();
+  const [filter1Checked, setfilter1Checked] = useState(false);
+  const [filter2Checked, setfilter2Checked] = useState(false);
+
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => {
+        setModalVisible(!modalVisible);
+      }}
+    >
+      <View className="h-full justify-center items-center bg-zinc-500/50">
+        <View className="bg-[#ece1e5] rounded-xl justify-between w-[65%] h-[230px]">
+          <View className="m-3">
+            <BoldText classNames="text-pink-900 text-[16px]">
+              ترتيب حسب :
+            </BoldText>
+            <View>
+              <RadioButton
+                color="rgb(131 24 67)"
+                value="السعر"
+                status={checked === "السعر" ? "checked" : "unchecked"}
+                onPress={() => setChecked("السعر")}
+              />
+              <RadioButton
+                color="rgb(131 24 67)"
+                value="التقييم"
+                status={checked === "التقييم" ? "checked" : "unchecked"}
+                onPress={() => setChecked("التقييم")}
+              />
+            </View>
+            <BoldText classNames="text-pink-900 text-[16px]">
+              تصفية حسب :
+            </BoldText>
+            <View>
+              <Checkbox
+                color="rgb(131 24 67)"
+                status={checked ? "checked" : "unchecked"}
+                onPress={() => {
+                  setChecked(!checked);
+                }}
+              />
+            </View>
+          </View>
+          <View className="flex-row gap-1 w-full p-1 pr-2">
+            <Pressable
+              className="items-center justify-center bg-pink-200/60 h-9 w-[49.5%] border border-zinc-100/50 rounded-lg"
+              onPress={() => setModalVisible(false)}
+            >
+              <BoldText classNames="text-[15px] text-pink-900">تطبيق</BoldText>
+            </Pressable>
+            <Pressable
+              className="items-center justify-center bg-pink-200/30 h-9 w-[49.5%] border border-zinc-100/50 rounded-lg"
+              onPress={() => setModalVisible(false)}
+            >
+              <BoldText classNames="text-[15px] text-pink-900">إلغاء</BoldText>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
--- components/Main/Header.js
+++ components/Main/Header.js
@@ -1,14 +1,13 @@
-import { Modal, Pressable, Text, View } from "react-native";
+import { Pressable, View } from "react-native";
 import { useSelected } from "../../lib/selectedContext";
 import { BoldText } from "../Text/BoldText";
 import { Filter } from "lucide-react-native";
+import { Filters } from "./Filters";
 import { useState } from "react";
-
-// sort : price, rating
-// filter : liked, offers
 
 export default function Header() {
   const { selected } = useSelected();
+
   const [modalVisible, setModalVisible] = useState(false);
 
   let Browser = [
@@ -34,37 +33,7 @@ export default function Header() {
           <Filter className="max-h-[18px]" fill="white" />
         </Pressable>
       )}
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <View className="h-full justify-center items-center bg-zinc-500/50">
-          <View className="bg-[#ece1e5] border border-zinc-500/30 rounded-xl w-[65%] h-[100px]">
-            <View className="flex-row gap-1 items-end h-full w-full p-1 pr-2">
-              <Pressable
-                className="items-center justify-center bg-pink-200/60 h-9 w-[49.5%] rounded-lg"
-                onPress={() => setModalVisible(false)}
-              >
-                <BoldText classNames="text-[15px] text-pink-900">
-                  تطبيق
-                </BoldText>
-              </Pressable>
-              <Pressable
-                className="items-center justify-center bg-pink-200/30 h-9 w-[49.5%] rounded-lg"
-                onPress={() => setModalVisible(false)}
-              >
-                <BoldText classNames="text-[15px] text-pink-900">
-                  إلغاء
-                </BoldText>
-              </Pressable>
-            </View>
-          </View>
-        </View>
-      </Modal>
+      <Filters modalVisible={modalVisible} setModalVisible={setModalVisible} />
       <BoldText classNames="text-lg text-white m-2">{selected}</BoldText>
     </View>
   );
